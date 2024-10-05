fcdf3aab3237559fb7cd9b8af2e0507a4273de7c
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-10-05 16:02:16 -0300

    Eliminando cosas innecesarias

--- src/web/formularios/editar_publi.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class EditarPubliForm(FlaskForm):
-    descripcion = StringField('Nueva Descripción', validators=[DataRequired(), Length(max=255)])
-    submit = SubmitField('Actualizar')
